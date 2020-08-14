380860c01ed2f76ea329060df6253e0183215f82
Edwin Vera <edwinvera@Edwins-MacBook-Air.local>
2020-08-14 12:21:59 -0400

    Connected muscles to movements

--- client/src/components/MovementDetail.jsx
+++ client/src/components/MovementDetail.jsx
@@ -12,9 +12,19 @@ export default function MovementDetail(props) {
     const movementDetail = await readOneMovement(props.match.params.id)
     setMovementDetail(movementDetail)
   }
-  
+
   return (
     <div>
+      {
+        movementDetail && (
+        <>
+          <h3>{movementDetail.name}</h3>
+          {movementDetail.muscles.map((muscle) => (
+            <p key={muscle.id}>{muscle.name}: {muscle.note}</p>
+          ))}
+        </>
+        )
+      }
       
     </div>
   )
