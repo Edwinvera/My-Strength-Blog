bcf33a0f3efad85284069c5587bf267755d61896
Edwin Vera <edwinvera@Edwins-MacBook-Air.local>
2020-08-18 13:07:04 -0400

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import { verifyUser } from './services/auth';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
       <Main
         setCurrentUser={setCurrentUser}
       />
+      <Footer />
     </div>
   );
 }
